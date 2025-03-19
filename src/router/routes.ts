a0c2069ee76d83d0aafce317927929837562cbe2
grez-lucas <lucasgrez3010@gmail.com>
2025-03-19 17:54:16 -0300

    feat(VerifyEmail): Add VerifyEmail page and route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -95,7 +95,16 @@ const routes: RouteRecordRaw[] = [
           title: 'Reset Password'
         },
         component: () => import('pages/auth/ResetPasswordPage.vue')
+      },
+      {
+        path: 'verify',
+        name: ROUTES_NAMES.verifyEmail,
+        meta: {
+          title: 'Verify Email'
+        },
+        component: () => import('pages/auth/VerifyEmailPage.vue')
       }
+
     ]
   },
 
