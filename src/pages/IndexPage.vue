1b296d6ccf2feb869372a09da1d7af6cd5ec8bd7
Diego Carhuavilca <diego.carhuavilca@gmail.com>
2025-03-21 15:57:57 -0500

    Update title

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -86,7 +86,7 @@
           </div>
         </div>
 
-        <div class="title">Host with the greates vulnerabilities</div>
+        <div class="title">Host with the greatest vulnerabilities</div>
 
         <q-list bordered>
           <template v-for="i in hostVulnerabilites" :key="i">
