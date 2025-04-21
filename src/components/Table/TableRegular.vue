162d334085c8914dbe6b595a3b8c33a062413110
Diego Carhuavilca <diego.carhuavilca@gmail.com>
2025-04-21 16:37:19 -0500

    Feat/vector drag (#53)
    * feat : Add wss report service
    
    * feat : Add Reports dynamic view
    
    * Update src/pages/ReportPage.vue
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>
    
    * feat : Update colors for cvss
    
    * feat : Add restart value behavior
    
    * fix : Updated overflow text in cards
    
    * fix :  Restart value behavior when apply
    
    ---------
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>

--- src/components/Table/TableRegular.vue
+++ src/components/Table/TableRegular.vue
@@ -1,10 +1,15 @@
 <template>
-  <q-table :rows="componentProps.rows" :columns="updateColumns" flat separator="none" style="max-height: 80vh"
-    virtual-scroll>
+  <q-table
+    :rows="componentProps.rows"
+    :columns="updateColumns"
+    flat
+    separator="none"
+    style="max-height: 80vh"
+    virtual-scroll
+  >
     <template #header-cell="props">
       <template v-if="props.col.name !== 'ID'">
-        <q-th :class="props.col.__thClass" class="table-header">
-          {{ props.col.name }}</q-th>
+        <q-th :class="props.col.__thClass" class="table-header"> {{ props.col.name }}</q-th>
       </template>
     </template>
     <template #body="props">
@@ -16,8 +21,15 @@
                 {{ col.value }}
               </slot>
               <template v-if="col.field === 'actions' && showActions">
-                <q-btn v-for="action in componentProps.actions" :key="action" :icon="getIcon(action)"
-                  class="button-table-action" dense flat @click="() => handlerEmitter(action, props.row)" />
+                <q-btn
+                  v-for="action in componentProps.actions"
+                  :key="action"
+                  :icon="getIcon(action)"
+                  class="button-table-action"
+                  dense
+                  flat
+                  @click="() => handlerEmitter(action, props.row)"
+                />
               </template>
             </q-td>
           </template>
@@ -28,85 +40,85 @@
 </template>
 
 <script setup lang="ts">
-import type { QTableColumn } from 'quasar';
-import { computed, onMounted, ref } from 'vue';
+  import type { QTableColumn } from 'quasar';
+  import { computed, onMounted, ref } from 'vue';
 
-type tableActions = 'edit' | 'delete' | 'insight' | 'search';
+  type tableActions = 'edit' | 'delete' | 'insight' | 'search' | 'detail';
 
-const componentProps = withDefaults(
-  defineProps<{
-    columns: QTableColumn[];
-    rows: Record<string, unknown>[];
-    actions?: tableActions[];
-    showActions?: boolean;
-  }>(),
-  {
-    actions: () => [],
-    showActions: true
+  const componentProps = withDefaults(
+    defineProps<{
+      columns: QTableColumn[];
+      rows: Record<string, unknown>[];
+      actions?: tableActions[];
+      showActions?: boolean;
+    }>(),
+    {
+      actions: () => [],
+      showActions: true
+    }
+  );
+
+  const emits = defineEmits<{
+    action: unknown;
+  }>();
+
+  const secondTick = ref(true);
+
+  const updateColumns = computed(() => {
+    const columns = [...componentProps.columns];
+    if (componentProps.actions?.length)
+      columns.push({
+        name: 'actions',
+        label: 'Actions',
+        align: 'center',
+        field: 'actions'
+      });
+    return columns;
+  });
+
+  function getIcon(action: tableActions): string {
+    switch (action) {
+      case 'edit':
+        return 'fas fa-pen-to-square';
+      case 'delete':
+        return 'fas fa-trash-can';
+      case 'insight':
+        return 'fas fa-chart-simple';
+      case 'search':
+        return 'fas fa-magnifying-glass';
+      default:
+        return 'fas fa-eye';
+    }
   }
-);
 
-const emits = defineEmits<{
-  action: unknown;
-}>();
-
-const secondTick = ref(true);
-
-const updateColumns = computed(() => {
-  const columns = [...componentProps.columns];
-  if (componentProps.actions?.length)
-    columns.push({
-      name: 'actions',
-      label: 'Actions',
-      align: 'center',
-      field: 'actions'
-    });
-  return columns;
-});
-
-function getIcon(action: tableActions): string {
-  switch (action) {
-    case 'edit':
-      return 'fas fa-pen-to-square';
-    case 'delete':
-      return 'fas fa-trash-can';
-    case 'insight':
-      return 'fas fa-chart-simple';
-    case 'search':
-      return 'fas fa-magnifying-glass';
-    default:
-      return '';
+  function handlerEmitter(action: tableActions, col: unknown): void {
+    emits('action', { action, col });
   }
-}
 
-function handlerEmitter(action: tableActions, col: unknown): void {
-  emits('action', { action, col });
-}
+  // function isDate(date: string): boolean {
+  //   return new Date(date).toString() !== 'Invalid Date';
+  // }
 
-// function isDate(date: string): boolean {
-//   return new Date(date).toString() !== 'Invalid Date';
-// }
+  // function formatDate(date: Date): string {
+  //   return new Date(date).toLocaleDateString();
+  // }
 
-// function formatDate(date: Date): string {
-//   return new Date(date).toLocaleDateString();
-// }
-
-onMounted(() => {
-  setInterval(function () {
-    secondTick.value = false;
-    secondTick.value = true;
-  }, 1000);
-});
+  onMounted(() => {
+    setInterval(function () {
+      secondTick.value = false;
+      secondTick.value = true;
+    }, 1000);
+  });
 </script>
 
 <style scoped>
-.button-table-action {
-  color: #5c7288;
-  font-size: 0.8em;
-}
+  .button-table-action {
+    color: #5c7288;
+    font-size: 0.8em;
+  }
 
-.table-header {
-  color: #5c7288;
-  text-transform: uppercase;
-}
+  .table-header {
+    color: #5c7288;
+    text-transform: uppercase;
+  }
 </style>
---
+++ src/models/wss-reports.models.ts
@@ -0,0 +1,105 @@
+export type WebSocketReportMessageRequest =
+  | {
+      type: WebSocketMessageRequest.INITIAL_DATA_REQUEST;
+      payload: {
+        scan_id: string;
+      };
+    }
+  | {
+      type: WebSocketMessageRequest.VECTOR_UPDATE_REQUEST;
+      payload: {
+        vulnerability_type_name: string;
+        new_value: number;
+      };
+    }
+  | {
+      type: WebSocketMessageRequest.VECTOR_SELECT_REQUEST;
+      payload: {
+        vulnerability_type_name: string;
+      };
+    }
+  | {
+      type: WebSocketMessageRequest.VECTOR_APPLY_REQUEST;
+      payload: object;
+    };
+
+export interface WebSocketInitialResponse {
+  type: WebSocketMessageType.INITIAL_DATA_RESPONSE;
+  payload: {
+    vulnerability_types: {
+      name: string;
+      highest_cvss: number;
+      count: number;
+      percentage: number;
+      available_cvss_values: number[];
+    }[];
+    global_cvss_score: number;
+    global_total_vulnerabilities: number;
+  };
+}
+
+export interface WebSocketInitialPayloadResponse {
+  vulnerability_types: {
+    name: string;
+    highest_cvss: number;
+    count: number;
+    percentage: number;
+    available_cvss_values: number[];
+  }[];
+  global_cvss_score: number;
+  global_total_vulnerabilities: number;
+}
+
+export type WebSocketReportIncomingMessage =
+  | WebSocketInitialResponse
+  | {
+      type: WebSocketMessageType.VECTOR_UPDATE_RESPONSE;
+      payload: {
+        expected_global_cvss_score: number;
+        expected_global_total_vulnerabilities: number;
+      };
+    }
+  | {
+      type: WebSocketMessageType.VECTOR_DETAILS_RESPONSE;
+      payload: {
+        vulnerability_details: {
+          id: string;
+          type: string;
+          cvss: number;
+          description: string;
+          [key: string]: unknown; // for optional additional fields
+        };
+      };
+    }
+  | {
+      type: WebSocketMessageType.REPORT_DATA_RESPONSE;
+      payload: {
+        solved_vulnerabilities: unknown[]; // You can define exact structure if needed
+        unattended_vulnerabilities: unknown[];
+        expected_security_posture: string;
+      };
+    }
+  | {
+      type: WebSocketMessageType.ERROR;
+      payload: {
+        message: string;
+      };
+    };
+
+export type WssMessageHandler = (data: WebSocketReportIncomingMessage) => void;
+
+export enum WebSocketMessageType {
+  INITIAL_DATA_RESPONSE = 'initial_data_response',
+  VECTOR_UPDATE_RESPONSE = 'vector_update_response',
+  VECTOR_DETAILS_RESPONSE = 'vector_details_response',
+  REPORT_DATA_RESPONSE = 'report_data_response',
+  ERROR = 'error'
+}
+
+export enum WebSocketMessageRequest {
+  INITIAL_DATA_REQUEST = 'initial_data_request',
+  VECTOR_UPDATE_REQUEST = 'vector_update',
+  VECTOR_SELECT_REQUEST = 'select_vector',
+  VECTOR_APPLY_REQUEST = 'apply_vectors_request',
+  ERROR = 'error'
+}
