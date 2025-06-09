d5d31d9f3988ca5e30b0bf623fcce126b989abef
Diego Carhuavilca <diego.carhuavilca@gmail.com>
2025-06-09 18:10:14 -0500

    chore : Update misc components for vulnerability

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,4 @@
-import SeverityChip from './shared/SeverityChip.vue';
-
-import ScanTableProgressBar from './scan/ScanTableProgressBar.vue';
 import ScanPickHostsStep from './scan/ScanPickHostsStep.vue';
 import ScanScheduleStep from './scan/ScanScheduleStep.vue';
 
-export { SeverityChip, ScanTableProgressBar, ScanPickHostsStep, ScanScheduleStep };
+export { ScanPickHostsStep, ScanScheduleStep };
