9198222a9a8822f80b3dfd792ca8a55353b42115
Diego Carhuavilca <diego.carhuavilca@gmail.com>
2025-08-26 18:55:49 -0500

    Aud 142 (#93)
    * feat: Add Module Selection View (Vulnerabilities and Audits)
    
    * feat: Add Module Selection View (Vulnerabilities and Audits)
    
    * feat: Add Module Selection View (Vulnerabilities and Audits)
    
    * Update src/modules/shared/pages/SelectModule.vue
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>
    
    * Update src/modules/shared/pages/SelectModule.vue
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>
    
    * Update src/modules/shared/pages/SelectModule.vue
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>
    
    * feat: Add Module Selection View (Vulnerabilities and Audits)
    
    * feat: Update pr title rules
    
    * feat: Update routes
    
    ---------
    
    Co-authored-by: Lucas Grez <lucasgrez3010@gmail.com>

--- src/modules/shared/routes/routes.ts
+++ src/modules/shared/routes/routes.ts
@@ -7,6 +7,7 @@ const sharedRoutes: RouteRecordRaw[] = [
     path: '/select-module',
     component: () => import('../pages/SelectModule.vue'),
     meta: {
+      requiresAuth: true,
       title: 'Select Module'
     }
   },
