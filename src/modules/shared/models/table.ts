94b66efbc679d90ea80a8f7e4991778b3201e6b4
Lucas Grez <lucasgrez3010@gmail.com>
2025-09-23 18:52:47 -0300

    feat: Add table tooltips and update ReportTable icons [VUL-27] (#127)
    * feat: Update app names to reflect Kriptome project naming update [VUL-29] (#124)
    
    fix(SelectModule): Update app names to reflect Kriptome project naming update
    
    * feat: Add table tooltips and update ReportTable icons [VUL-27] (#126)
    
    feat: Add table tooltips and update ReportTable icons

--- src/modules/shared/models/table.ts
+++ src/modules/shared/models/table.ts
@@ -4,6 +4,8 @@ export enum TableActions {
   INSIGHT = 'insight',
   SEARCH = 'search',
   DETAIL = 'detail',
-  INFORMATION = 'information'
+  INFORMATION = 'information',
+  VIEW_ASSETS = 'view_assets',
+  POLAR_REPORT = 'polar_report'
 }
 export type tableActions = TableActions;
