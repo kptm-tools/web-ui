790f4f11ae036ac446b04cb5dc49628075de0191
Diego Carhuavilca <diego.carhuavilca@gmail.com>
2025-10-06 11:09:16 -0500

    feat: Update view of functions table

--- src/modules/audits/pages/AdminFunctionView.vue
+++ src/modules/audits/pages/AdminFunctionView.vue
@@ -1,36 +1,83 @@
 <template>
   <div class="container q-pa-md">
     <audits-tabs></audits-tabs>
-    <template v-if="pickedView === ViewOptions.FUNCTION">
-      <q-table
-        flat
-        title="Funciones"
-        :rows="functionsRows"
-        row-key="code"
-        @row-click="selectCategoryView"
-      />
-    </template>
+    <div class="table-container q-mt-md">
+      <template v-if="pickedView === ViewOptions.FUNCTION">
+        <div class="row q-pa-md text-weight-bold border-bottom border-red">
+          <div class="col-3 flex items-center">Codigo</div>
+          <div class="col-3 flex items-center">Nombre</div>
+          <div class="col-3 flex items-center">Descripcion</div>
+          <div class="col-3"></div>
+        </div>
 
-    <template v-else-if="pickedView === ViewOptions.CATEGORY">
-      <q-table
-        flat
-        title="Categorias"
-        :rows="categoryRows"
-        row-key="code"
-        @row-click="selectSubCategoryView"
-      />
-      <q-btn label="Atras" color="primary" class="q-my-md" @click="selectFunctionView"></q-btn>
-    </template>
+        <q-card
+          v-for="(row, index) in functionsRows"
+          :key="row.code"
+          class="row q-pa-md table-function-card q-my-md bg-grey-2 cursor-pointer"
+          @click="selectCategoryView(index)"
+        >
+          <div class="col-3 flex items-center">{{ row.code }}</div>
+          <div class="col-3 flex items-center">{{ row.name }}</div>
+          <div class="col-3 flex items-center">{{ row.description }}</div>
+          <div class="col-3 flex items-center justify-center">
+            <q-btn icon="chevron_right" flat></q-btn>
+          </div>
+        </q-card>
+      </template>
 
-    <template v-else-if="pickedView === ViewOptions.SUBCATEGORY">
-      <q-table flat title="Subcategorias" :rows="subCategoryRows" row-key="code" class="q-my-md" />
-      <q-btn
-        label="Atras"
-        color="primary"
-        class="q-my-md"
-        @click="selectCategoryView(null, null, pickedCategoryIndex)"
-      ></q-btn>
-    </template>
+      <template v-else-if="pickedView === ViewOptions.CATEGORY">
+        <div class="row q-pa-md text-weight-bold border-bottom border-red">
+          <div class="col-3 flex items-center">Codigo</div>
+          <div class="col-3 flex items-center">Nombre</div>
+          <div class="col-3 flex items-center">Descripcion</div>
+          <div class="col-3"></div>
+        </div>
+
+        <q-card
+          v-for="(row, index) in categoryRows"
+          :key="row.code"
+          class="row q-pa-md table-function-card q-my-md bg-grey-2 cursor-pointer"
+          @click="selectSubCategoryView(index)"
+        >
+          <div class="col-3 flex items-center">{{ row.code }}</div>
+          <div class="col-3 flex items-center">{{ row.name }}</div>
+          <div class="col-3 flex items-center">{{ row.description }}</div>
+          <div class="col-3 flex items-center justify-center">
+            <q-btn icon="chevron_right" flat></q-btn>
+          </div>
+        </q-card>
+        <q-btn label="Atras" color="primary" class="q-my-md" @click="selectFunctionView"></q-btn>
+      </template>
+
+      <template v-else-if="pickedView === ViewOptions.SUBCATEGORY">
+        <div class="row q-pa-md text-weight-bold border-bottom border-red">
+          <div class="col-3 flex items-center">ID</div>
+          <div class="col-3 flex items-center">Codigo</div>
+          <div class="col-3 flex items-center">Descripcion</div>
+          <div class="col-3 flex items-center"></div>
+        </div>
+
+        <q-card
+          v-for="row in subCategoryRows"
+          :key="row.code"
+          class="row q-pa-md table-function-card q-my-md bg-grey-2 cursor-pointer"
+        >
+          <div class="col-3 flex items-center">{{ row.id }}</div>
+          <div class="col-3 flex items-center">{{ row.code }}</div>
+          <div class="col-3 flex items-center">{{ row.description }}</div>
+          <div class="col-3 flex items-center justify-center">
+            <q-btn icon="chevron_right" flat></q-btn>
+          </div>
+        </q-card>
+
+        <q-btn
+          label="Atras"
+          color="primary"
+          class="q-my-md"
+          @click="selectCategoryView(pickedCategoryIndex)"
+        ></q-btn>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -57,12 +104,12 @@
     pickedView.value = ViewOptions.FUNCTION;
   }
 
-  function selectCategoryView(event: Event | null, row: null, index: number): void {
+  function selectCategoryView(index: number): void {
     frameworkStore.pickFunctionIndex(index);
     pickedView.value = ViewOptions.CATEGORY;
   }
 
-  function selectSubCategoryView(event: Event | null, row: null, index: number): void {
+  function selectSubCategoryView(index: number): void {
     frameworkStore.pickCategoryIndex(index);
     pickedView.value = ViewOptions.SUBCATEGORY;
   }
@@ -71,3 +118,9 @@
     await frameworkStore.fetchStructure();
   });
 </script>
+
+<style lang="scss">
+  .table-function-card {
+    border: 1px solid rgba(30, 30, 48, 0.397);
+  }
+</style>
